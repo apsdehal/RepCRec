4fc95a8af775bc00402985d8de91a824512afd5f
Amanpreet Singh <apsdehal@gmail.com>
2017-10-30 19:42:19 -0400

    Add base line reader to IO

--- RepCRec/IO.py
+++ RepCRec/IO.py
@@ -1,12 +1,32 @@
 import os
 
+from .Instruction import Instruction
+
 
 class IO:
-    def __init__(self, file):
-        self.file_name = file
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.line_generator = self._get_line_generator()
 
     def get_next_instruction(self):
-        return None
+        line = next(self.line_generator, None)
+
+        if line is None:
+            return line
+        else:
+            return self._process_instruction(line)
+
+    def _get_line_generator(self):
+        with open(self.file_name, 'r') as input_file:
+            for line in input_file:
+                if len(line) > 1:
+                    yield line
+
+    def _process_instruction(self, line):
+        line = line.strip().split(";")
+        instructions = []
+        for instruction in line:
+            instructions.append(Instruction(instruction))
 
     def print_all_variables(self, data_table):
         return None
