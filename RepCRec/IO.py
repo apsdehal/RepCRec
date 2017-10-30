164c9a13ece186414d681202c0947d8a4fe24b0a
Amanpreet Singh <apsdehal@gmail.com>
2017-10-30 01:57:46 -0400

    Add barebones IO class

---
+++ RepCRec/IO.py
@@ -0,0 +1,18 @@
+import os
+
+
+class IO:
+    def __init__(self, file):
+        self.file_name = file
+
+    def get_next_instruction(self):
+        return None
+
+    def print_all_variables(self, data_table):
+        return None
+
+    def print_variable(self, variable):
+        return None
+
+    def print_site(self, site):
+        return None
