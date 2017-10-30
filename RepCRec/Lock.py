6402220c8f0c1955a89adb3eb28990b1f30b022d
Amanpreet Singh <apsdehal@gmail.com>
2017-10-30 02:02:14 -0400

    Add barebones Lock class
    - Has Transaction and LockType for particular lock

---
+++ RepCRec/Lock.py
@@ -0,0 +1,29 @@
+from .enums.LockType import LockType
+from .Transaction import Transaction
+
+class Lock:
+    def __init__(self, lock_type=None, transaction=None):
+        # TODO Think if we need an id for a lock
+        self.lock_type = lock_type
+        self.transaction = transaction
+
+    def get_transaction(self):
+        return self.transaction
+
+    def get_lock_type(self):
+        return self.lock_type
+
+    def set_lock_type(self, lock_type):
+        if lock_type in LockType:
+            self.lock_type = lock_type
+        else:
+            # TODO: Throw some kind of error here
+            return
+
+    def set_transaction(self, transaction=None):
+        if isinstance(transaction, Transaction) \
+           or transaction is None:
+            self.transaction = transaction
+        else:
+            # TODO throw some kind of error here also
+            return
