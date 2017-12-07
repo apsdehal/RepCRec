3bde45376e32c42751521aa1c9a698711fc362e9
Amanpreet Singh <apsdehal@gmail.com>
2017-12-07 03:36:20 -0500

    Fix test runner and show proper diffs

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,39 +1,72 @@
 import subprocess
 import os
+import plac
 from os import path
+from termcolor import colored
+import difflib
 
-# input_files = [f for f in os.listdir('inputs/') if path.isfile(f)]
-# output_files = [f for f in os.listdir('outputs/') if path.isfile(f)]
-
-input_files = os.listdir('tests/inputs/')
-output_files = os.listdir('tests/outputs/')
-
-input_files.sort()
-output_files.sort()
+INPUT_FOLDER = 'tests/inputs/'
+OUTPUT_FOLDER = 'tests/outputs/'
 
 
-for inp_file, out_file in zip(input_files, output_files):
+def color_diff(diff):
+    for line in diff:
+        if line.startswith('+'):
+            yield colored(line, "green")
+        elif line.startswith('-'):
+            yield colored(line, "red")
+        elif line.startswith('^'):
+            yield colored(line, "blue")
+        else:
+            yield line
 
-    os.system('python -m RepCRec.start tests/inputs/' + str(inp_file))
 
-    my_output = str()
-    test_output = str()
-    new_test_output = str()
+@plac.annotations(
+    file_path=("Intermediatery save file name",
+               "positional", None, str))
+def main(file_path="./output.txt"):
+    input_files = os.listdir(INPUT_FOLDER)
+    output_files = os.listdir(OUTPUT_FOLDER)
 
-    with open('my_output.txt', 'r') as outfile, open('tests/outputs/' + str(out_file), 'r') as myfile:
+    input_files.sort()
+    output_files.sort()
 
-        result = outfile.readlines()
-        test_output = myfile.readlines()
+    d = difflib.Differ()
 
-        for line1, line2 in zip(result, test_output):
+    for inp_file, out_file in zip(input_files, output_files):
 
-            my_output += line1.split('-')[1]
-            new_test_output += line2.split("-")[1]
+        os.system('python -m RepCRec.start -o ' + file_path + ' ' +
+                  INPUT_FOLDER + str(inp_file))
 
-            if my_output != new_test_output:
-                print(line1, line2)
+        actual_output = []
+        expected_output = str()
+        stripped_expected_output = []
 
-    if my_output == new_test_output:
-        print("Success")
-    else:
-        print("Failed " + str(inp_file))
+        with open(file_path, 'r') as outfile, \
+                open(OUTPUT_FOLDER + str(out_file), 'r') as test_file:
+
+            result = outfile.readlines()
+            expected_output = test_file.readlines()
+            for line1, line2 in zip(result, expected_output):
+
+                line1 = line1.split('-')[-1]
+                actual_output.append(line1)
+                line2 = line2.split("-")[-1]
+                stripped_expected_output.append(line2)
+
+        diff = difflib.ndiff(actual_output, stripped_expected_output)
+        diff_final = []
+        for d in diff:
+            if d.startswith('+') or d.startswith('-'):
+                diff_final.append(d)
+        diff = diff_final
+        print("\n".join(color_diff(diff)))
+
+        if actual_output == stripped_expected_output:
+            print(colored(str(inp_file) + ": Success", "green"))
+        else:
+            print(colored(str(inp_file) + ": Failed", "red"))
+
+
+if __name__ == '__main__':
+    plac.call(main)
