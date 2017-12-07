6a7edb172d53b3e182bcce4f24a1b1671c370658
Amanpreet Singh <apsdehal@gmail.com>
2017-12-07 02:57:04 -0500

    Add more options to start

--- RepCRec/start.py
+++ RepCRec/start.py
@@ -8,41 +8,62 @@ from .SiteManager import SiteManager
 from .TransactionManager import TransactionManager
 from .IO import IO
 from .LockTable import LockTable
+from tornado.ioloop import IOLoop
+from tornado import gen
 
 
 class Main:
 
     @plac.annotations(
-        file_path=("File name", "positional", None, str),
+        file_path=("File name, pass anything in case of stdin",
+                   "positional", None, str),
         num_sites=("Number of Sites", "option", "n", int),
-        num_variables=("Number of variables", "option", "v", int))
+        num_variables=("Number of variables", "option", "v", int),
+        sites=("Whether to bring sites up", "flag", "s"),
+        out_file=("Output file, if not passed by default we" +
+                  "will print to stdout", "option", "o", str),
+        stdin=("Takes input from stdin instead of file if passed", "flag",
+               "i"))
     def __init__(self, file_path,
                  num_sites=config['NUM_SITES'],
-                 num_variables=config['NUM_VARIABLES']):
-
+                 num_variables=config['NUM_VARIABLES'],
+                 sites=False,
+                 out_file=None,
+                 stdin=False):
         p = Path('.')
         p = p / file_path
 
+        if out_file:
+            open(out_file, 'w').close()
+
+        logging.basicConfig(filename=out_file,
+                            format='%(levelname)s - %(asctime)s - %(message)s',
+                            level=config['LOG_LEVEL'])
+
         self.site_manager = SiteManager(num_sites, num_variables)
+
         self.lock_table = LockTable()
 
         self.transaction_manager = TransactionManager(
             num_variables, num_sites, self.lock_table, self.site_manager)
 
         self.io = IO(p, self.site_manager,
-                     self.transaction_manager, self.lock_table)
+                     self.transaction_manager, self.lock_table, stdin=stdin)
+        self.sites = sites
 
+    @gen.coroutine
     def run(self):
-        # self.site_manager.start()
+        if self.sites:
+            self.site_manager.start()
+
         self.io.run()
 
 
 if __name__ == "__main__":
-
-    open('my_output.txt', 'w').close()
-
-    logging.basicConfig(filename='my_output.txt', format='%(levelname)s - %(asctime)s - %(message)s',
-                        level=config['LOG_LEVEL'])
-
     main = plac.call(Main)
-    main.run()
+
+    if main.sites:
+        IOLoop.instance().add_callback(main.run)
+        IOLoop.current().start()
+    else:
+        main.run()
