b534a67696c20df054995068529dcaf0c586c11f
Amanpreet Singh <apsdehal@gmail.com>
2017-12-12 20:30:01 -0500

    Change default log level to INFO
    So graders don't see different results than us.

--- RepCRec/sample.config.py
+++ RepCRec/sample.config.py
@@ -4,7 +4,7 @@ import logging
 # TODO: For now, use this later shift to argparser
 config = {
     "BASE_PORT": 9919,
-    "LOG_LEVEL": logging.DEBUG,
+    "LOG_LEVEL": logging.INFO,
     "NUM_SITES": 10,
     "NUM_VARIABLES": 20
 }
